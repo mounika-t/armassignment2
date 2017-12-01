4ab7677a90acccf22e6ce6294959555b6b1cff14
mounika-t <33395198+mounika-t@users.noreply.github.com>
2017-12-02 00:13:59 +0530

    Update expoFP.s

--- expoFP.s
+++ expoFP.s
@@ -12,13 +12,13 @@ __main  FUNCTION
 	VLDR.F32 s2,=1	;s2 stores sum of the series
 loop1	CMP r2,r1      ; compare R1 and R2
 	BNE LOOP       ; if count isless than no of terms go to LOOP	
-	
+	B stop
 LOOP   VMUL.F32 s1,s1,s0       ;s1=s1*s0
-       VMOV.F32 s5,r1    ;moving the bit stream in r1 to s5(FPR)
+       VMOV.F32 s5,r2    ;moving the bit stream in r2 to s5(FPR)
 	VCVT.F32.U32 s5,s5 ;converting bitsream to unsigned  32 bit fp no 
 	VDIV.F32 s1,s1,s5    ;divide s1 by count and store it back to s1	
 	VADD.F32 s2,s2,s1  ; add sum and s1 and store it in sum
-	ADD R1,R1,#1	;increement count
+	ADD R2,R2,#1	;increement count
 	B loop1
 stop B stop
      ENDFUNC
